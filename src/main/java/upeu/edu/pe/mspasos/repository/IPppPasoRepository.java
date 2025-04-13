4c54217cff1532a4354aefed7f1265ea92410d01
PIERO\LENOVO <Andres>
2025-04-13 16:08:23 -0500

    first commit

--- src/main/java/upeu/edu/pe/mspasos/repository/IPppPasoRepository.java
+++ src/main/java/upeu/edu/pe/mspasos/repository/IPppPasoRepository.java
@@ -1,8 +1,10 @@
 package upeu.edu.pe.mspasos.repository;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import upeu.edu.pe.mspasos.domain.Pasos_ppp;
 
 import java.util.List;
-public interface IPppPasoRepository {
+
+public interface IPppPasoRepository extends JpaRepository<Pasos_ppp, Long> {
     List<Pasos_ppp> findByPppId(Long pppId);
 }
--- src/main/java/upeu/edu/pe/mspasos/service/impl/IPppPasoServiceImpl.java
+++ src/main/java/upeu/edu/pe/mspasos/service/impl/IPppPasoServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import upeu.edu.pe.mspasos.domain.Pasos_ppp;
 import upeu.edu.pe.mspasos.repository.IPppPasoRepository;
+import upeu.edu.pe.mspasos.service.IPppPasoService;
 
 
 import java.util.List;
